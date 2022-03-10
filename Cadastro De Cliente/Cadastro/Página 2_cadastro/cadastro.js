8287f8203eb821fa1279170f4cd940139f1c34a8
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-09 23:01:41 -0300

    comandos e style

---
+++ Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
@@ -0,0 +1,12 @@
+let buttom = document.getElementById('buttom')
+
+function entrar(){
+    buttom.setAttribute('style', 'background-color:rgb(248, 236, 236)')
+}
+function sair(){
+    buttom.setAttribute('style', 'background-color:white')
+}
+
+function Adicionar(){
+    alert('olada certo ate aqui')
+}
