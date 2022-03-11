a2ceca6de1ba5212e0201b0deadc65b417ccd940
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-10 22:54:25 -0300

    validação
    Adicionando as funcionalidade de validação

--- Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
+++ Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
@@ -4,24 +4,24 @@ const idade = document.getElementById('age')
 const email = document.getElementById('email')
 const end = document.getElementById('end')
 const data_nasc = document.getElementById('nasc')
-//-----------------------------------------------------
-let alert_nome = document.getElementById('nome')
-const alert_idade = document.getElementById("idade")
+//---------------------alertas--------------------------------
+const alert_nome = document.getElementById("nome")
+const alert_idade = document.getElementById('idade')
 const alert_mail = document.getElementById('mail')
 const alert_end = document.getElementById('end')
 const alert_nasc = document.getElementById('nasc')
+//---------------------validadores-----------------------
+let n = false
+let i = false
+let e = false
+let E = false
+let d = false
 
-function entrar(){
-    buttom.setAttribute('style', 'background-color:rgb(248, 236, 236)')
-}
-function sair(){
-    buttom.setAttribute('style', 'background-color:white')
-}
 
 function Adicionar(){
-    alert("ola")
-    if(nome.value.length<7){
-        alert_nome('style', 'color: red')
-        
-    }
+    if(nome.value.length < 7){
+        alert_nome.innerText='*O nome invalido, tente colocar seu nome de novo.'
+        alert_nome.setAttribute('style','font-size: 10pt')
+    }else{ n = true}
+    
 }
