5c10589238c608eb0c45237721dbbcbfb26da20f
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-10 23:12:01 -0300

    adicionando mais validações

--- Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
+++ Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
@@ -8,8 +8,8 @@ const data_nasc = document.getElementById('nasc')
 const alert_nome = document.getElementById("nome")
 const alert_idade = document.getElementById('idade')
 const alert_mail = document.getElementById('mail')
-const alert_end = document.getElementById('end')
-const alert_nasc = document.getElementById('nasc')
+const alert_end = document.getElementById('endereco')
+const alert_nasc = document.getElementById('data_nasc')
 //---------------------validadores-----------------------
 let n = false
 let i = false
@@ -20,8 +20,28 @@ let d = false
 
 function Adicionar(){
     if(nome.value.length < 7){
-        alert_nome.innerText='*O nome invalido, tente colocar seu nome de novo.'
+        alert_nome.innerText='*O nome é invalido, tente novamente.'
         alert_nome.setAttribute('style','font-size: 10pt')
     }else{ n = true}
+
+    if(idade.value.length <= 0){
+        alert_idade.setAttribute('style', 'font-size: 10pt')
+        alert_idade.innerText = '*A idade está errada, tente novamente.'
+    }else{ i = true}
+
+    if(email.value.length <= 0){
+        alert_mail.setAttribute('style', 'font-size: 10pt')
+        alert_mail.innerText = '*o e-mail está incorreto, tente novamente.'
+    }else{ e = true}
     
+    if(end.value.length <= 0){
+        alert_end.setAttribute('style', 'font-size: 10pt')
+        alert_end.innerText = '*O endereço está incorreto, tente novamente.'
+    }else{ E = true}
+
+    if(data_nasc.value.length <= 0){
+        alert_nasc.setAttribute('style', 'font-size: 10pt')
+        alert_nasc.innerText = '*a data de nascimente está incorreta, tente novamente. '
+    }else{ d = true}
+
 }
