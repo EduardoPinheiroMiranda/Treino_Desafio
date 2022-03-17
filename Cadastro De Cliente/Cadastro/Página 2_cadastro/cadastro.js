7ffd81365c41864e3c808f60684f4ec5dbc06ce0
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-16 23:07:39 -0300

    New update
    Foi adicionado e corrigidos os validadores das respostas e também foi criado um mini BDA no local host.

--- Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
+++ Cadastro De Cliente/Cadastro/Página 2_cadastro/cadastro.js
@@ -11,37 +11,73 @@ const alert_mail = document.getElementById('mail')
 const alert_end = document.getElementById('endereco')
 const alert_nasc = document.getElementById('data_nasc')
 //---------------------validadores-----------------------
-let n = false
-let i = false
-let e = false
-let E = false
-let d = false
-
+let n 
+let i 
+let e 
+let em 
+let d
 
 function Adicionar(){
     if(nome.value.length < 7){
         alert_nome.innerText='*O nome é invalido, tente novamente.'
         alert_nome.setAttribute('style','font-size: 10pt')
-    }else{ n = true}
+        n = 'false'
+    }else{ 
+        n = 'true'
+        alert_nome.innerHTML = ''
+    }
 
-    if(idade.value.length <= 0){
+    if(idade.value <= 0){
         alert_idade.setAttribute('style', 'font-size: 10pt')
         alert_idade.innerText = '*A idade está errada, tente novamente.'
-    }else{ i = true}
+        i = 'false'
+    }else{ 
+        i = 'true'
+        alert_idade.innerHTML = ''
+    }
 
     if(email.value.length <= 0){
         alert_mail.setAttribute('style', 'font-size: 10pt')
         alert_mail.innerText = '*o e-mail está incorreto, tente novamente.'
-    }else{ e = true}
+        e = 'false'
+    }else{ 
+        e = 'true'
+        alert_mail.innerHTML = ''
+    }
     
     if(end.value.length <= 0){
         alert_end.setAttribute('style', 'font-size: 10pt')
         alert_end.innerText = '*O endereço está incorreto, tente novamente.'
-    }else{ E = true}
+        em = 'false'
+    }else{ 
+        em = 'true'
+        alert_end.innerHTML = ''
+    }
 
-    if(data_nasc.value.length <= 0){
+    if(data_nasc.value <= 1011900 ){
         alert_nasc.setAttribute('style', 'font-size: 10pt')
         alert_nasc.innerText = '*a data de nascimente está incorreta, tente novamente. '
-    }else{ d = true}
+        d = 'false'
+    }else{ 
+        d = 'true'
+        alert_nasc.innerHTML = ''
+    }
+
+    
+    if(n=='true' && i=='true' && e=='true' && em=='true' && d=='true'){
+        let listadecadastro = JSON.parse(localStorage.getItem('listadecadastro')||'[]')
+        listadecadastro.push({
+            nome: nome.value,
+            idade:idade.value,
+            email:email.value,
+            end:end.value,
+            data_nasc: data_nasc.value
+        })
+        localStorage.setItem('listadecadastro', JSON.stringify(listadecadastro))
+        alert('dados salvos')
+
+    }
+
+
 
 }
