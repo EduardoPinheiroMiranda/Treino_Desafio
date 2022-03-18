c428b08449ae6a29f77960d270fe0dc276cf9ee6
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-17 23:22:57 -0300

    new add

--- Cadastro De Cliente/Cadastro/Página 1/TabelaEcadastro.js
+++ Cadastro De Cliente/Cadastro/Página 1/TabelaEcadastro.js
@@ -1,3 +1,12 @@
+let clientes = document.getElementById('TabeladePessoas')
+
+    listadecadastro = JSON.parse(localStorage.getItem('listadecadastro'))
+    listadecadastro.forEach((item) => {
+        if(item.nome != ' '){
+            clientes.innerHTML += `${item.nome} | ${item.idade}|${item.email} | ${item.end} | ${item.data_nasc}<br>`
+        }
+    });
+
 function Adicionar(){
-    window.location.href="../Página 2_cadastro/cadastro.html"
+    window.location.href = "../Página 2_cadastro/cadastro.html"
 }
