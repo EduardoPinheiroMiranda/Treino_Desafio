26b955d343615c14d244a0a5068959399619abc2
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-17 23:59:07 -0300

    update

--- Cadastro De Cliente/Cadastro/Página 1/TabelaEcadastro.js
+++ Cadastro De Cliente/Cadastro/Página 1/TabelaEcadastro.js
@@ -1,11 +1,13 @@
-let clientes = document.getElementById('TabeladePessoas')
+let clientes = document.getElementById('TabeladePessoas');
 
     listadecadastro = JSON.parse(localStorage.getItem('listadecadastro'))
-    listadecadastro.forEach((item) => {
-        if(item.nome != ' '){
-            clientes.innerHTML += `${item.nome} | ${item.idade}|${item.email} | ${item.end} | ${item.data_nasc}<br>`
-        }
-    });
+        listadecadastro.forEach((item) => {
+            if(item.nome != ' '){
+                clientes.innerHTML += `${item.nome} | ${item.idade}|${item.email} | ${item.end} | ${item.data_nasc}<br>`
+            }
+        });
+
+    
 
 function Adicionar(){
     window.location.href = "../Página 2_cadastro/cadastro.html"
