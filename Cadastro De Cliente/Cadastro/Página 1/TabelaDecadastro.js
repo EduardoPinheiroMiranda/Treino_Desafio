ad98869fd57b38e6d593226d1e1b1e66f6f7b13e
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-24 23:44:03 -0300

    crindo tabelas dinamicamentes
    Foi introduzidos os valores adquiridos na página de cadastro a uma tabela de forma dinamicamente.

---
+++ Cadastro De Cliente/Cadastro/Página 1/TabelaDecadastro.js
@@ -0,0 +1,24 @@
+
+    let table = document.getElementById("table")
+
+        let listadecadastro = JSON.parse(localStorage.getItem('listadecadastro'))
+            listadecadastro.forEach((item) => {
+                let tr = table.insertRow()
+
+                let td_Nome = tr.insertCell(0)
+                let td_idade = tr.insertCell(1)
+                let td_email = tr.insertCell(2)
+                let td_endereco = tr.insertCell(3)
+                let td_data_nasc = tr.insertCell(4)
+                
+                td_Nome.innerText = item.nome
+                td_idade.innerText = item.idade
+                td_email.innerText = item.email
+                td_endereco.innerText = item.endereco
+                td_data_nasc.innerText = item.data_nasc
+            });
+
+
+function Adicionar(){
+    window.location.href = "../Página 2_cadastro/cadastro.html"
+}
